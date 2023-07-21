d5a2816f5f79cc9b240ed2007283b2bbb8d96283
aminedjohar <aminedjohar@gmail.com>
2023-07-21 11:48:40 +0100

    backing up the authentification

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,6 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
-import Home from "../views/Home";
 import { Link } from 'react-router-dom'
 
 
@@ -11,10 +10,10 @@ export default function GuestLayout() {
   if (userToken) {
     return <Navigate to="/" />
   }
-
+  
   return (
     <div>
-      {/* <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div><Link to="/home">
             <img
@@ -25,8 +24,7 @@ export default function GuestLayout() {
           </div>
           <Outlet />
         </div>
-      </div> */}
-      <Home />
+      </div>
     </div>
   )
 }
